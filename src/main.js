50fb60089dd6be11f119a5f5f9a3040d7abb305e
Vsevolod Ivanov <www@sevolod.ru>
2016-01-04 00:23:53 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@
 const CompletionProvider = require('./completion-provider');
 
 class AutocompleteModulesPlugin {
+  constructor() {
+    this.config = {
+      title: 'Offset Folder for locals',
+      description: 'Offset Folder for locals',
+      type: 'string',
+      'default': '',
+      order: 1
+    }; 
+  }
+  
   activate() {
     this.completionProvider = new CompletionProvider();
   }
